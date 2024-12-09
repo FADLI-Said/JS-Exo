0878da7839c407db6829d092a68755dbda7dd467
FADLI Saïd <saidfadli224@gmail.com>
2024-12-09 09:22:41 +0100

    Update = -> ==

--- ExoJS-part1&part2/script.js
+++ ExoJS-part1&part2/script.js
@@ -223,13 +223,13 @@
 //     if (number%3!=0 & number%5!=0 & (number%3!=0 & number%5!=0)) {
 //         console.log(number);
 //     }
-//     if (number%3===0 & number%5!=0) {
+//     if (number%3==0 & number%5!==0) {
 //         console.log("Fizz");
 //     }
-//     if (number%5===0 & number%3!=0) {
+//     if (number%5==0 & number%3!==0) {
 //         console.log("Buzz");
 //     }
-//     if (number%3===0 & number%5===0) {
+//     if (number%3==0 & number%5==0) {
 //         console.log("FizzBuzz");    
 //     }
 // }
